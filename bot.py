e53be521ba3d86f61fba7a78e34e8486bacf285b
Steven Parker <voodoonofx@gmail.com>
2017-04-04 12:31:15 -0700

    Moving logic to bot.py. What the fuck was I thinking putting it all in the gui.

--- bot.py
+++ bot.py
@@ -1,16 +1,50 @@
-import roplus
+import itertools
+from datetime import datetime
 
+import roplus
 from gui import main_window
+
+import BigWorld
 
 class Bot(object):
     def __init__(self):
         roplus.log('AutoLoot Loading')
-        self.running = False
+
+        # A few defaults in case onPulse hasn't triggered yet.
+        self.player = self.p = BigWorld.player()
+        self.entities = {}
+        self.items = []
+
+        # Some configuration options
+        self.entity_range = 100
+
+        # And some internals
+        self.last_loot_attempt_time = datetime.now()
+        self.key_func = lambda e: e.__module__
+
         self.mainWindow = main_window.MainWindow(self)
         self.mainWindow.show()
         roplus.registerCallback('ROPlus.OnPulse', self.onPulseCallback)
         roplus.log(' ... Done')
 
-    def onPulseCallback(self, args):
-        if self.running:
-            self.engine.pulse()
+    def onPulseCallback(self, *args, **kw):
+        # roplus.log('self: {0}'.format(self), 'args: {0}'.format(args), 'kw: {0}'.format(kw))
+        grouper = itertools.groupby(sorted(self.p.entitiesInRange(self.entity_range), key=self.key_func), key=self.key_func)
+        self.entities = dict((k, list(v)) for (k, v) in grouper)
+        self.items = self.entities.get('DroppedItem', []) + self.entities.get('TreasureBox', [])
+        # roplus.log('self.enabled_auto_loot:', self.enabled_auto_loot)
+        for item in self.items:
+            if not item:
+                continue
+            # Loot it, if we can
+            item_dist = self.p.position.distTo(item.position)
+            if item_dist < 4.0 and self.mainWindow.enabled_auto_loot:
+                # Try looting it!
+                if (datetime.now() - self.last_loot_attempt_time).total_seconds() > 1:
+                    if item.__module__ == 'TreasureBox':
+                        roplus.log('Opening Treasure Box: {0}'.format(item.roleName))
+                        item.use()
+                    elif item.__module__ == 'DroppedItem' and item._checkPickItem(self.p):
+                        roplus.log('Looting Nearby Item: {0}'.format(item.roleName))
+                        self.p.pickNearByItems(True)
+                    self.last_loot_attempt_time = datetime.now()
--- gui/main_window.py
+++ gui/main_window.py
@@ -1,5 +1,4 @@
 
-import itertools
 from datetime import datetime
 
 import BigWorld
@@ -14,44 +13,14 @@ class MainWindow(object):
     def __init__(self, botInstance):
         self.bot = botInstance
         self.window_visable = False
-        self.entity_range = 100
         self.enabled_auto_loot = False
-        self.last_loot_attempt_time = datetime.now()
-        self.key_func = lambda e: e.__module__
-        self.player = self.p = BigWorld.player()
 
-        # A few defaults in case onPulse hasn't triggered yet.
-        self.entities = {}
-        self.items = []
 
         # Register functions
         roplus.registerCallback('ROPlus.OnDrawGUI', self.onDrawGuiCallback)
-        roplus.registerCallback('ROPlus.OnPulse', self.onPulseCallback)
 
     def show(self):
         self.window_visable = True
-
-    def onPulseCallback(self, *args, **kw):
-        # roplus.log('self: {0}'.format(self), 'args: {0}'.format(args), 'kw: {0}'.format(kw))
-        grouper = itertools.groupby(sorted(self.p.entitiesInRange(self.entity_range), key=self.key_func), key=self.key_func)
-        self.entities = dict((k, list(v)) for (k, v) in grouper)
-        self.items = self.entities.get('DroppedItem', []) + self.entities.get('TreasureBox', [])
-        # roplus.log('self.enabled_auto_loot:', self.enabled_auto_loot)
-        for item in self.items:
-            if not item:
-                continue
-            # Loot it, if we can
-            item_dist = self.p.position.distTo(item.position)
-            if item_dist < 4.0 and self.enabled_auto_loot:
-                # Try looting it!
-                if (datetime.now() - self.last_loot_attempt_time).total_seconds() > 1:
-                    if item.__module__ == 'TreasureBox':
-                        roplus.log('Opening Treasure Box: {0}'.format(item.roleName))
-                        item.use()
-                    elif item.__module__ == 'DroppedItem' and item._checkPickItem(self.p):
-                        roplus.log('Looting Nearby Item: {0}'.format(item.roleName))
-                        self.p.pickNearByItems(True)
-                    self.last_loot_attempt_time = datetime.now()
 
     def onDrawGuiCallback(self, *args, **kw):
         if self.window_visable:
@@ -61,9 +30,9 @@ class MainWindow(object):
                     if imgui.checkbox('Enable Auto-Loot', self.enabled_auto_loot):
                         self.enabled_auto_loot = not self.enabled_auto_loot
                     
-                    if imgui.collapsingHeader('Available Loot ({0} items)'.format(len(self.items))):
+                    if imgui.collapsingHeader('Available Loot ({0} items)'.format(len(self.bot.items))):
                         imgui.columns(4)
-                        for item in self.items:
+                        for item in self.bot.items:
                             if not item:
                                 continue
 
@@ -72,14 +41,14 @@ class MainWindow(object):
 
                             if item.__module__ == 'DroppedItem':
                                 try:
-                                    imgui.text('Lootable' if item._checkPickItem(self.p) else 'Not Lootable')
+                                    imgui.text('Lootable' if item._checkPickItem(self.bot.p) else 'Not Lootable')
                                 except AttributeError:
                                     imgui.text('Not _checkPickItem')
                             else:
                                 imgui.text('Openable?')
                             imgui.nextColumn()
 
-                            imgui.text('{0}'.format(self.p.position.distTo(item.position)))
+                            imgui.text('{0}'.format(self.bot.p.position.distTo(item.position)))
                             imgui.nextColumn()
 
                             if imgui.button('Go To {0}##NavToEntity'.format(item.__module__)):
@@ -89,7 +58,7 @@ class MainWindow(object):
                         imgui.columns(1)
 
                     if imgui.collapsingHeader('Debug All Entities'):
-                        for entity_name, entities in sorted(self.entities.iteritems()):
+                        for entity_name, entities in sorted(self.bot.entities.iteritems()):
                             for entity in entities:
                                 imgui.columns(5)
                                 imgui.separator()
@@ -97,15 +66,15 @@ class MainWindow(object):
                                 imgui.nextColumn()
                                 imgui.text('{0}'.format(entity.id))
                                 imgui.nextColumn()
-                                if entity_name == 'DroppedItem' and hasattr(entity, '_checkPickItem') and entity._checkPickItem(self.p):
+                                if entity_name == 'DroppedItem' and hasattr(entity, '_checkPickItem') and entity._checkPickItem(self.bot.p):
                                     imgui.text('{0}'.format('Lootable'))
                                 elif not entity_name == 'DroppedItem':
                                     imgui.text('No Data Available')
                                 else:
                                     imgui.text('Not your Loot!')
                                 imgui.nextColumn()
-                                if entity and hasattr(entity, 'position') and self.p and hasattr(self.p, 'position'):
-                                    imgui.text('{0}'.format(self.p.position.distTo(entity.position)))
+                                if entity and hasattr(entity, 'position') and self.bot.p and hasattr(self.bot.p, 'position'):
+                                    imgui.text('{0}'.format(self.bot.p.position.distTo(entity.position)))
                                 else:
                                     imgui.text('No Position Information')
                                 imgui.nextColumn()
